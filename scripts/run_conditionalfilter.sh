78c00d46e4df179e578c85b6731a2a0363377f15
MGordon09 <54448110+MGordon09@users.noreply.github.com>
2021-05-20 13:55:24 +0100

    Update run_conditionalfilter.sh
    Adjust prevalence parameter

--- scripts/run_conditionalfilter.sh
+++ scripts/run_conditionalfilter.sh
@@ -15,7 +15,7 @@ mkdir -p ${ANALYSIS_FOLDER}/deblur/filtered_frequency_table/
 
 qiime feature-table filter-features-conditionally \
  --i-table ${ANALYSIS_FOLDER}/deblur/table-deblur.qza \
- --p-prevalence 0 \
+ --p-prevalence 0.38 \ 
  --p-abundance 0.00005 \
  --o-filtered-table ${ANALYSIS_FOLDER}/deblur/filtered_frequency_table/filtered_table.qza 
 
